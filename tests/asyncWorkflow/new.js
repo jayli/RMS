ba00e1d741d05efd793aaf01546ca00b4538dd8d
yyfrankyy <yyfrankyy@gmail.com>
2011-08-04 17:26:55 +0800

    add some event on asyncWorkflow

--- tests/asyncWorkflow/new.js
+++ tests/asyncWorkflow/new.js
@@ -1,13 +1,34 @@
 // vim: set sw=2 ts=2:
 
-var aw = require('../../lib/asyncWorkflow');
+var aw = require('../../lib/asyncWorkflow'),
+    assert = require('assert');
 
-var wf = new aw('中文', {
+assert.throws(function() {
+  new aw('aa', {
     type: 'string',
     steps: [
-        ['nativeascii', {}],
-        ['syntaxChecker', {}]
+      ['eascii', {}]
     ]
-});
+  });
+}, aw.JOB_NOT_SUPPORTED);
 
+var wf = new aw('中文', {
+  type: 'string',
+  steps: [
+    ['nativeascii', {}]
+  ]
+});
+wf.on('start', function() {
+  console.info('AsyncWorkflow Start..');
+});
+wf.on('stepStart', function(e) {
+  console.info(' [%s] %s', this.current, this.steps[this.current].name);
+  console.info('   [INPUT]: %s', e.input);
+});
+wf.on('stepEnd', function(e) {
+  console.info('   [OUTPUT]: %s', e.output);
+});
+wf.on('end', function(e) {
+  console.info('DONE.');
+});
 wf.start();
