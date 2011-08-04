47d4268f1da273e724a5db4797f4702933764b8d
yyfrankyy <yyfrankyy@gmail.com>
2011-08-04 18:30:20 +0800

    add some more specs close #3

---
+++ tests/asyncWorkflow.js
@@ -0,0 +1,29 @@
+// vim: set sw=2 ts=2:
+
+var aw = require('../lib/asyncWorkflow'),
+    assert = require('assert');
+
+var wf;
+
+assert.throws(function() {
+  wf = new aw('aa', {
+    type: 'string',
+    steps: [
+      ['eascii', {}]
+    ]
+  });
+}, aw.JOB_NOT_SUPPORTED);
+
+wf = new aw('中文', {
+  type: 'string',
+  steps: [
+    ['nativeascii', {}]
+  ]
+});
+wf.on('stepStart', function(e) {
+  assert.equal(e.input, '中文');
+});
+wf.on('stepEnd', function(e) {
+  assert.equal(e.output, '\\u4E2D\\u6587');
+});
+wf.start();
