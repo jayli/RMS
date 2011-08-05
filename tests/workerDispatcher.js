01095e547804744a4ef2d17fe052fc35c7d63a32
yyfrankyy <yyfrankyy@gmail.com>
2011-08-05 17:51:33 +0800

    adjust workerdispatcher api

--- tests/workerDispatcher.js
+++ tests/workerDispatcher.js
@@ -1,10 +1,19 @@
 // vim: set sw=2 ts=2:
 
-var dispatcher = require('../lib/workerDispatcher');
+var util = require('util');
+var assert = require('assert');
+var Worker = require('../lib/workerDispatcher');
 
-new dispatcher('aa', {
+var worker = new Worker('aa', {
   type: 'string',
   steps: [
     ['nativeascii', {}]
   ]
 });
+
+worker.on('message', function(e) {
+  assert.equal('aa', e.data.result);
+  assert.ok(e.data.success);
+});
+
+worker.dispatch();
