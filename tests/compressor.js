8ccdf07937164196e2902954c5b9a99af0f53900
yyfrankyy <yyfrankyy@gmail.com>
2011-08-16 23:02:00 +0800

    add more test data

--- tests/compressor.js
+++ tests/compressor.js
@@ -6,38 +6,48 @@ var http = require('http');
 var querystring = require('querystring');
 var util = require('util');
 
-var content = path.join(__dirname, 'data', 'kissy.js');
-content = fs.readFileSync(content).toString();
-
-var postData = querystring.stringify({
-  content: content,
-  config: JSON.stringify({
-    type: 'JavaScript',
-    steps: [['compressor', {}]]
-  })
-});
-
-var opts = {
-  host: 'trade.ued.taobao.net',
-  port: 8080,
-  path: '/precompile',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Content-Length': postData.length
-  }
-};
-
-var postReq = http.request(opts, function(res) {
-  res.setEncoding('utf8');
-  var code = '';
-  res.on('data', function(chunk) {
-    code += chunk.toString();
+function run(content, type) {
+  var postData = querystring.stringify({
+    content: content,
+    config: JSON.stringify({
+      type: type,
+      steps: [['compressor', {}]]
+    })
   });
-  res.on('end', function(chunk) {
-    console.log(code);
-  });
-});
 
-postReq.write(postData);
-postReq.end();
+  var opts = {
+    host: 'trade.ued.taobao.net',
+    port: 8080,
+    path: '/precompile',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': postData.length
+    }
+  };
+
+  var postReq = http.request(opts, function(res) {
+    res.setEncoding('utf8');
+    var code = '';
+    res.on('data', function(chunk) {
+      code += chunk.toString();
+    }).on('end', function(chunk) {
+      console.log('Done.');
+    });
+  });
+
+  postReq.write(postData);
+  postReq.end();
+}
+
+
+function read(name) {
+  var content = path.join(__dirname, 'data', name);
+  return fs.readFileSync(content).toString();
+}
+
+//run(read('kissy.js'), 'JavaScript');
+//run(read('kissy-min.js'), 'JavaScript');
+//run(read('search.css'), 'CSS');
+// encode problem
+run(read('search.source.css'), 'CSS');
