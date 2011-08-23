641da94ed404a8ad06f2d3564b90e7dacf3b5ed6
yyfrankyy <yyfrankyy@gmail.com>
2011-08-23 16:58:10 +0800

    done with worker timeout test specs, close #24, close #25

--- tests/workerDispatcher.js
+++ tests/workerDispatcher.js
@@ -3,17 +3,34 @@
 var util = require('util');
 var assert = require('assert');
 var Worker = require('../lib/workerDispatcher');
+var rmsConfig = require('../lib/rmsConfig');
 
-var worker = new Worker('aa', {
+var w1 = new Worker('aa', {
   type: 'string',
   steps: [
     ['nativeascii', {}]
   ]
 });
-
-worker.on('message', function(e) {
-  assert.equal('aa', e.data.result);
-  assert.ok(e.data.success);
+w1.on('message', function(data) {
+  assert.equal('aa', data.result);
+  assert.ok(data.success);
+  w1.worker.terminate();
 });
+w1.dispatch();
 
-worker.dispatch();
+// timeout
+/*
+rmsConfig.workerTimeout = 10;
+var w2 = new Worker('aa', {
+  type: 'string',
+  steps: [
+    ['nativeascii', {}]
+  ]
+});
+w2.on('message', function(data) {
+  assert.equal('aa', data.result);
+  assert.ok(!data.success);
+  w2.worker.terminate();
+});
+w2.dispatch();
+*/
