02e08cebe2d2acf86158282def4aea53d94c29ed
yyfrankyy <yyfrankyy@gmail.com>
2011-11-26 22:13:30 +0800

    start transform makefile to javascript

---
+++ index.js
@@ -0,0 +1,22 @@
+// vim: set sw=2 ts=2:
+
+/**
+ * @fileoverview Resource Management System.
+ * @author yyfrankyy@gmail.com (Frank Xu)
+ */
+
+var forever = require('forever');
+var rmsUtil = require('./lib/rmsUtil');
+
+function deploy(type) {
+  forever[rmsUtil.producing ? 'startDaemon' : 'start'](
+      __dirname + '/lib/' + type + '/index.js');
+}
+
+var target = process.argv[2];
+if (target) {
+  deploy(target);
+} else {
+  deploy('service');
+  deploy('web');
+}
